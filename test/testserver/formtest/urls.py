b23369e72b5b0b983a641fc273347d2db19e0575
synw <synwx@protonmail.com>
2023-09-05 15:56:23 +0200

    [NEW] Add more tests

--- test/testserver/formtest/urls.py
+++ test/testserver/formtest/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import form_view
+from .views import (
+    form_view,
+    form_view_with_status_codes,
+    server_error,
+    form_view_custom_response,
+)
 
 
 urlpatterns = [
     path("", form_view, name="index"),
+    path("status", form_view_with_status_codes, name="status"),
+    path("error", server_error, name="error"),
+    path("custom", form_view_custom_response, name="custom"),
 ]
--- test/testserver/formtest/views.py
+++ test/testserver/formtest/views.py
@@ -1,21 +1,34 @@
 import json
-from django.http import JsonResponse
+from django.http import HttpRequest, HttpResponseServerError, JsonResponse, HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
 from django.views.decorators.csrf import csrf_exempt
+from pydantic import ValidationError
+from .schemas import LoginFormContract
 
 
 @csrf_exempt
-def form_view(request):
+def form_view(request: HttpRequest) -> HttpResponse:
     json_data = request.body.decode("utf-8")
     data = json.loads(json_data)
 
+    # validate the data vs the schema
+    try:
+        LoginFormContract(**data)
+    except ValidationError as e:
+        return JsonResponse({"error": {"type": "schema"}, "errors": e.errors()})
+
+    # process form
     form = AuthenticationForm(data=data)
     if form.is_valid() is False:
         return JsonResponse(
-            {"errors": form.errors.get_json_data(escape_html=True)},  # type: ignore
+            {
+                "error": {"type": "validation"},
+                "errors": form.errors.get_json_data(escape_html=True),  # type: ignore
+            },
         )
 
+    # authenticate user
     user = authenticate(
         username=form.cleaned_data.get("username"),
         password=form.cleaned_data.get("password"),
@@ -23,4 +36,79 @@ def form_view(request):
     if user is not None:
         login(request, user)
 
-    return JsonResponse({"errors": []})
+    return HttpResponse(status=204)
+
+
+@csrf_exempt
+def form_view_with_status_codes(request: HttpRequest) -> HttpResponse:
+    json_data = request.body.decode("utf-8")
+    data = json.loads(json_data)
+
+    # validate the data vs the schema
+    try:
+        LoginFormContract(**data)
+    except ValidationError as e:
+        return JsonResponse(
+            {"error": {"type": "schema"}, "errors": e.errors()}, status=418
+        )
+
+    # process form
+    form = AuthenticationForm(data=data)
+    if form.is_valid() is False:
+        return JsonResponse(
+            {
+                "error": {"type": "validation"},
+                "errors": form.errors.get_json_data(escape_html=True),  # type: ignore
+            },
+            status=422,
+        )
+
+    # authenticate user
+    user = authenticate(
+        username=form.cleaned_data.get("username"),
+        password=form.cleaned_data.get("password"),
+    )
+    if user is not None:
+        login(request, user)
+
+    return HttpResponse(status=204)
+
+
+@csrf_exempt
+def server_error(request: HttpRequest) -> HttpResponse:
+    return HttpResponseServerError()
+
+
+@csrf_exempt
+def form_view_custom_response(request: HttpRequest) -> HttpResponse:
+    json_data = request.body.decode("utf-8")
+    data = json.loads(json_data)
+
+    # validate the data vs the schema
+    try:
+        LoginFormContract(**data)
+    except ValidationError as e:
+        return JsonResponse(
+            {"error": {"type": "schema"}, "errors": e.errors()}, status=418
+        )
+
+    # process form
+    form = AuthenticationForm(data=data)
+    if form.is_valid() is False:
+        return JsonResponse(
+            {
+                "error": {"type": "validation"},
+                "errors": form.errors.get_json_data(escape_html=True),  # type: ignore
+            },
+            status=422,
+        )
+
+    # authenticate user
+    user = authenticate(
+        username=form.cleaned_data.get("username"),
+        password=form.cleaned_data.get("password"),
+    )
+    if user is not None:
+        login(request, user)
+
+    return JsonResponse({"extra": "foo"})
